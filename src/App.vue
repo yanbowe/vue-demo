c1413bf7a7437c5f9661ad1bbad471dfc28a1884
燕博文 <349952469@qq.com>
2023-05-11 16:42:11 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+import Comp from './Comp.vue'
+
+const count = ref(1)
+</script>
+
+<template>
+  <Comp v-model="count"/>
+</template>
---
+++ src/Comp.vue
@@ -0,0 +1,11 @@
+<template>
+  <div @click="add">{{ modelValue }}</div>
+</template>
+
+<script setup>
+const modelValue = defineModel({ required: true });
+function add() {
+  modelValue.value += 1;
+}
+</script>
+<style scoped></style>
